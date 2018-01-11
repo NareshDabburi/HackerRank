242d4562d58ea3fad282f262bec718504790623f
NareshDabburi <35326127+NareshDabburi@users.noreply.github.com>
2018-01-12 00:39:28 +0530

    Create Solution.java

---
+++ Algorithms/Warmup/Staircase/Solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=n-i;j>=1;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+               // for(int l=0; l>k; l++){
+                    System.out.print("#");
+               // }
+            }
+            System.out.println();
+        }
+            
+    }
+}
